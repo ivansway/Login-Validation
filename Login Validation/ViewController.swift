095229cf3bfcc62ae252355866187a97183c0ed7
Burnspb <djsky7777@gmail.com>
2020-08-19 19:03:49 +0300

    Initial Commit

---
+++ Login Validation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Validation
+//
+//  Created by Ivan Myrza on 19/08/2020.
+//  Copyright © 2020 Ivan Myrza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
